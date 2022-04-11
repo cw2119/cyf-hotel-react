efdcecb1a35217155633fb9b2e7b1739327bc47e
cw2119 <47484939+cw2119@users.noreply.github.com>
2022-04-11 17:37:26 +0100

    part two - changing wording on website

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <header className="App-header">Code-Your-Future Hotel App</header>
       <Bookings />
     </div>
   );
